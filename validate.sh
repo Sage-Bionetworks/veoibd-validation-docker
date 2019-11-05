c8e8f913035b5c380ac3b115b3b45666f1d5c317
kdaily <kenneth.daily@sagebase.org>
2019-11-05 13:40:39 -0800

    add config and get ec2 region

--- validate.sh
+++ validate.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Getting region
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+
 # Get Synapse config file from SSM
-aws ssm get-parameters --names veoibd-aws-infra/synapseConfig --with-decryption --region us-east-1 --output text --query "Parameters[*].{Value:Value}" > /root/.synapseConfig
+aws ssm get-parameters --names veoibd-aws-infra/synapseConfig --with-decryption --region ${EC2_REGION} --output text --query "Parameters[*].{Value:Value}" > /root/.synapseConfig
 
 # Run the validation proceess
-input_to_database.py main --onlyValidate
+input_to_database.py --config /config.json main --onlyValidate
