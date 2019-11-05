01dea113d5a4039b15a2f7bef922085438ff122b
kdaily <kenneth.daily@sagebase.org>
2019-11-05 14:54:41 -0800

    fix name of ssm parameter

--- validate.sh
+++ validate.sh
@@ -5,7 +5,7 @@ EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availa
 EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
 
 # Get Synapse config file from SSM
-aws ssm get-parameters --names veoibd-aws-infra/synapseConfig --with-decryption --region ${EC2_REGION} --output text --query "Parameters[*].{Value:Value}" > /root/.synapseConfig
+aws ssm get-parameters --names /veoibd-aws-infra/synapseConfig --with-decryption --region ${EC2_REGION} --output text --query "Parameters[*].{Value:Value}" > /root/.synapseConfig
 
 # Run the validation proceess
 input_to_database.py --format_registry_packages veoibddatasubmission --config /config.json --onlyValidate main
